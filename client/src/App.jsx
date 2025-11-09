85778e9aa6bbb8ecc20aaa76b33fcc24627f2fba
sudikshavoleti <sudikshav6@gmail.com>
2025-11-09 15:08:08 +0530

    Updated UI and bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "https://mern-todo-backend.onrender.com/todos"; 
+const API_URL = "https://mern-m28a.onrender.com"; 
 // ⬆️ Replace this with your real Render backend URL
 
 export default function App() {
